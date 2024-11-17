2e201b9d2a8516bc3836fe48647027513ebf188b
cwesly <cauawesly@outlook.com>
2024-11-17 10:26:16 -0300

    chore: add and configure gemini Api

---
+++ src/hooks/useGeminiApi.ts
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import { API_KEY } from '../constants';
+
+const useGeminiApi = (name: string, trigger: boolean = false) => {
+  const [meaning, setMeaning] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const genAI = new GoogleGenerativeAI(API_KEY);
+      const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
+      const prompt = `Em uma frase, explique o significado do nome ${name}`;
+
+      try {
+        const result = await model.generateContent(prompt);
+        setMeaning(result.response.text());
+      } catch (err: any) {
+        if (err instanceof Error){
+          setError(err.message); 
+        } else {
+           setError('Ocorreu um erro desconhecido.'); 
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (name?.trim() && trigger) {
+      setLoading(true);
+      fetchData();
+    }
+  }, [name, trigger]);
+
+  return { meaning, loading, error };
+};
+
+export default useGeminiApi;
+
